0c9ea57513a46e8cf01374667d8a81572b4d40d1
Gavin H <gavinmajetich@gmail.com>
2022-03-15 18:18:25 -0400

    Set up thread spawners for C# r-w

--- Classical-Problems/Readers-Writers/readerswriters.cs
+++ Classical-Problems/Readers-Writers/readerswriters.cs
@@ -1,11 +1,89 @@
 using System;
 using System.Threading;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 
-namespace readerswriters {
-    class ReadersWritersClass {
-        static void Main(string[] args) {
-            
+public class Reader {
+    private int id;
+    public Reader(int i) {
+        this.id = i;
+    }
+
+    public void run() {
+        Console.WriteLine($"Reader {id} arrives");
+    }
+}
+
+public class Writer {
+    private int id;
+    public Writer(int i) {
+        this.id = i;
+    }
+    public void run() {
+        Console.WriteLine($"Writer {id} arrives");
+    }
+}
+
+public class Spawners {
+    public static void ReaderSpawner(int numReaders) {
+        Thread[] threads = new Thread[numReaders];
+
+        // Spawn given number of threads
+        for (int i = 0; i < numReaders; i++) {
+            Reader r = new Reader(i);
+            Thread t = new Thread(new ThreadStart(r.run));
+            t.Start();
+            threads[i] = t;
         }
+
+
+        // Join all threads we made
+        foreach (var t in threads)
+        {
+            t.Join();
+        }
+    }
+
+    public static void WriterSpawner(int numWriters) {
+        // This might need to be atomic so it doesn't get starved by reader spawner
+        Thread[] threads = new Thread[numWriters];
+
+        // Spawn given number of threads
+        for (int i = 0; i < numWriters; i++) {
+            Writer w = new Writer(i);
+            Thread t = new Thread(new ThreadStart(w.run));
+            t.Start();
+            threads[i] = t;
+        }
+
+
+        // Join all threads we made
+        foreach (var t in threads)
+        {
+            t.Join();
+        }
+    }
+}
+public class ReadersWritersClass {
+    ReadersWritersClass(int r, int w) {
+        ThreadStart readerTS = delegate {
+            Spawners.ReaderSpawner(r);
+        };
+
+        ThreadStart writerTS = delegate {
+            Spawners.WriterSpawner(w);
+        };
+        Thread readerSpawner = new Thread(readerTS);
+        Thread writerSpawner = new Thread(writerTS);
+
+        readerSpawner.Start();
+        writerSpawner.Start();
+
+        readerSpawner.Join();
+        writerSpawner.Join();
+    }
+    public static void Main(string[] args) {
+        // Use command line args soon
+        new ReadersWritersClass(3,1);
     }
 }
