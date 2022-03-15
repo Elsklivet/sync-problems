4f1bf16efc4237c65d13781a620494975fbc2709
Gavin H <gavinmajetich@gmail.com>
2022-03-15 16:49:00 -0400

    Initializing C# r-w sol

---
+++ Classical-Problems/Readers-Writers/readerswriters.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Threading;
+using System.Collections.Concurrent;
+
+namespace readerswriters {
+    class ReadersWritersClass {
+        static void Main(string[] args) {
+            
+        }
+    }
+}
