fe4a8d6968d5958bf614c51eeb05e035d716702e
Gavin H <gavinmajetich@gmail.com>
2022-03-24 13:29:53 -0400

    Finally got C# r-w sol to work

--- Classical-Problems/Readers-Writers/readerswriters.cs
+++ Classical-Problems/Readers-Writers/readerswriters.cs
@@ -6,48 +6,99 @@ using System.Collections.Generic;
 public class ReadersWritersClass {
 
     // Shared variable hours
-    private int writersInside = 0;
-    private int readersInside = 0;
-    private int data = 0;
+    internal int writersInside = 0;
+    internal int readersInside = 0;
+    internal int data = 0;
 
     // Time for sync primitives
-    internal static Mutex mutex;
-    internal static AutoResetEvent writerCanEnter;
-    internal static AutoResetEvent readerCanEnter;
+    internal Mutex mutex;
+    internal AutoResetEvent writerCanEnter;
+    internal ManualResetEvent readerCanEnter;
 
 
-    public class Reader {
+    internal class Reader {
         private int id;
-        public Reader(int i) {
+        private ReadersWritersClass upper;
+        public Reader(int i, ReadersWritersClass upper) {
             this.id = i;
+            this.upper = upper;
         }
 
         public void run() {
             Console.WriteLine($"Reader {id} arrives");
 
-            mutex.WaitOne();
-            // crit
-            mutex.ReleaseMutex();
+            upper.mutex.WaitOne();
+            // Need to check if a writer is in the room
+            while ( upper.writersInside > 0 ) {
+                upper.mutex.ReleaseMutex();
+                upper.readerCanEnter.WaitOne();
+                upper.mutex.WaitOne();
+            }
+            Console.WriteLine($"Reader {id} enters the room");
+            upper.readersInside++;
+            upper.mutex.ReleaseMutex();
+
+            upper.mutex.WaitOne();
+            Console.WriteLine($"Reader {id} reads {upper.data} from shared data");
+            upper.mutex.ReleaseMutex();
+
+            upper.mutex.WaitOne();
+            Console.WriteLine($"Reader {id} leaves the room");
+            upper.readersInside--;
+            if ( upper.readersInside == 0 ) {
+                upper.mutex.ReleaseMutex();
+                upper.writerCanEnter.Set();
+                return;
+            }
+            upper.mutex.ReleaseMutex();
         }
     }
 
-    public class Writer {
+    internal class Writer {
         private int id;
-        public Writer(int i) {
+        private ReadersWritersClass upper;
+        public Writer(int i, ReadersWritersClass upper) {
             this.id = i;
+            this.upper = upper;
         }
         public void run() {
             Console.WriteLine($"Writer {id} arrives");
+
+            upper.mutex.WaitOne();
+            while ( upper.writersInside > 0 || upper.readersInside > 0 ) {
+                upper.mutex.ReleaseMutex();
+                upper.writerCanEnter.WaitOne();
+                upper.mutex.WaitOne();
+            }
+            Console.WriteLine($"Writer {id} enters the room");
+            upper.writersInside++;
+            upper.writerCanEnter.Reset();
+            upper.mutex.ReleaseMutex();
+
+            upper.mutex.WaitOne();
+            upper.data++;
+            Console.WriteLine($"Writer {id} wrote {upper.data} to shared data");
+            upper.mutex.ReleaseMutex();
+
+            upper.mutex.WaitOne();
+            Console.WriteLine($"Writer {id} leaves the room");
+            upper.writersInside--;
+            upper.readerCanEnter.Set();
+            upper.writerCanEnter.Set();
+            upper.mutex.ReleaseMutex();
         }
     }
 
-    public class Spawners {
-        public static void ReaderSpawner(int numReaders) {
+    internal class Spawners {
+        public Spawners(){
+            // do nothing
+        }
+        public void ReaderSpawner(int numReaders, ReadersWritersClass upper) {
             Thread[] threads = new Thread[numReaders];
 
             // Spawn given number of threads
             for (int i = 0; i < numReaders; i++) {
-                Reader r = new Reader(i);
+                Reader r = new Reader(i, upper);
                 Thread t = new Thread(new ThreadStart(r.run));
                 t.Start();
                 threads[i] = t;
@@ -61,13 +112,13 @@ public class ReadersWritersClass {
             }
         }
 
-        public static void WriterSpawner(int numWriters) {
+        public void WriterSpawner(int numWriters, ReadersWritersClass upper) {
             // This might need to be atomic so it doesn't get starved by reader spawner
             Thread[] threads = new Thread[numWriters];
 
             // Spawn given number of threads
             for (int i = 0; i < numWriters; i++) {
-                Writer w = new Writer(i);
+                Writer w = new Writer(i, upper);
                 Thread t = new Thread(new ThreadStart(w.run));
                 t.Start();
                 threads[i] = t;
@@ -85,14 +136,19 @@ public class ReadersWritersClass {
     ReadersWritersClass(int r, int w) {
         this.mutex = new Mutex();
         this.writerCanEnter = new AutoResetEvent(false);
-        this.readerCanEnter = new AutoResetEvent(true);
+        this.readerCanEnter = new ManualResetEvent(true);
+        this.data = 0;
+        this.writersInside = 0;
+        this.readersInside = 0;
+
+        Spawners spawner = new Spawners();
 
         ThreadStart readerTS = delegate {
-            Spawners.ReaderSpawner(r);
+            spawner.ReaderSpawner(r, this);
         };
 
         ThreadStart writerTS = delegate {
-            Spawners.WriterSpawner(w);
+            spawner.WriterSpawner(w, this);
         };
         Thread readerSpawner = new Thread(readerTS);
         Thread writerSpawner = new Thread(writerTS);
@@ -104,7 +160,12 @@ public class ReadersWritersClass {
         writerSpawner.Join();
     }
     public static void Main(string[] args) {
+        int r = 3, w = 1;
         // Use command line args soon
-        new ReadersWritersClass(3,1);
+        if ( args.Length >= 2 ) {
+            r = int.Parse(args[0]);
+            w = int.Parse(args[1]);
+        }
+        new ReadersWritersClass(r,w);
     }
 }
